2b44fe3102bfced26b8494148e285dbb881b59f7
jackonprogramming <joaquin_santoyo@hotmail.com>
2021-09-19 17:00:09 -0500

    push changes

--- server.js
+++ server.js
@@ -1,50 +1,22 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
-const router = express.Router();
+const bodyParser = require("body-parser");
 
 const response = require("./Network/response");
-
-const PORT = process.env.PORT || 8080;
+const router = require("./Network/Messages/network");
+// const PORT = process.env.PORT || 8080;
 
 var app = express();
 
+app.use(router);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(router);
 
-app.listen(PORT, console.log(`server started on port ${PORT}`));
-
-router.post("/post", (req, res) => {
-  console.log("Connected to React");
-  response.success(req, res, "hello react");
-});
-
-router.get("/message", function (req, res) {
-  //   console.log(req.headers);
-  //   res.send("hello handsome, are using the method get");
-
-  //   res.status(200).send({ response: true, body: [1, 2, 3] });
-  response.success(req, res, "hello from get method");
-});
-
-router.post("/message", function (req, res) {
-  // query work with de URL params
-
-  //   console.log(req.query);
-  //   console.log(req.body);
-  //   res.send("hello handsome, are posting");
-
-  if (req.query.error == "ok") {
-    response.error(req, res, "this is a error fucking monkey", 404);
-  } else {
-    response.success(req, res, "hello from post method", 201);
-  }
-});
+// app.listen(PORT, console.log(`server started on port ${PORT}`));
 
 // app.use("/", function (req, res) {
 //   res.send("holis wapox");
 // });
 
-app.use("/app", express.static("public"));
+// app.use("/app", express.static("public"));
 
 app.listen(3000);
